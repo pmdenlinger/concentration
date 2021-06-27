02b16c1ace3adde418c7c9fbb3b7b7c9cd734b8e
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-27 08:16:17 -0700

    Initial Commit

---
+++ concentration/concentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  concentrationApp.swift
+//  concentration
+//
+//  Created by user on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct concentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
