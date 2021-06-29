86e0e392b6533465b3dafae70f5e69cfc875be2c
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-28 18:24:02 -0700

    Create ConcentrationGame model.

--- concentration/ConcentrationGame.swift
+++ concentration/ConcentrationGame.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct ConcentrationGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+    
+}
